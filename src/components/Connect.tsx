569b370710c0bf4d14374c6d0fd74b3e60c5815b
s t e i n <stein@stein.rip>
2023-08-18 18:02:38 -0400

    connect card done

--- src/components/Connect.tsx
+++ src/components/Connect.tsx
@@ -1,7 +1,22 @@
 import "./Connect.css";
 
 const Connect = () => {
-	return <div className="Connect">Connect works</div>;
+	return (
+		<div className="Connect">
+			<header>
+				<h1>Follow Gamp Camp on social media</h1>
+			</header>
+			<div className="Socials">
+				<a
+					className="TikTok"
+					target="_blank"
+					rel="noopener noreferrer"
+					href="https://www.tiktok.com/@gampcamp?lang=en">
+					TikTok
+				</a>
+			</div>
+		</div>
+	);
 };
 
 export default Connect;
