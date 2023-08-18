8f2ca385599f6d0173f9d1bc9d371c7474d22461
s t e i n <stein@stein.rip>
2023-08-17 20:24:08 -0400

    removed home border and added more to press

--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -11,18 +11,35 @@ const NewsCard = () => {
 					Abortion-rights advocates see increase in volunteers after Supreme
 					Court decision
 				</h1>
+				<img
+					src="https://images.radio.com/aiu-media/IMG0372-f9a42a28-440d-473d-bce4-cf4a8561b843.jpg?width=800"
+					alt="React Image"
+				/>
 				<h2>EVE SAMPSON, DETROIT FREE PRESS</h2>
-				<p>read more</p>
+				<a
+					target="_blank"
+					rel="noopener noreferrer"
+					href="https://www.freep.com/restricted/?return=https%3A%2F%2Fwww.freep.com%2Fstory%2Fnews%2Flocal%2Fmichigan%2Fdetroit%2F2022%2F07%2F06%2Fabortion-clinic-escorts-see-volunteers-increase%2F7804617001%2F">
+					read more
+				</a>
 			</div>
 			<div className="Article 2">
 				<h1>
 					Rainbow umbrellas, kazoos and tenacity: On the front line with Metro
 					Detroit abortion clinic defenders
 				</h1>
+				<img
+					src="https://images.radio.com/aiu-media/IMG0372-f9a42a28-440d-473d-bce4-cf4a8561b843.jpg?width=800"
+					alt="React Image"
+				/>
 				<h2>EMILY ROTH, WWJ NEWSRADIO 950</h2>
-				<p>read more</p>
+				<a
+					target="_blank"
+					rel="noopener noreferrer"
+					href="https://www.audacy.com/wwjnewsradio/news/local/on-front-line-with-metro-detroit-abortion-clinic-defenders">
+					read more
+				</a>
 			</div>
-			{/* <img src="Favicon.ico" alt="Gamp Camp Logo" /> */}
 		</div>
 	);
 };
